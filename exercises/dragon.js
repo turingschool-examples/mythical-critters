302a0dc0231a95df1937b16852d222b90d6a0980
Noah Gambee <noah@gambee.dev>
2019-10-14 08:05:40 -0600

    Finish tests 1-6

---
+++ exercises/dragon.js
@@ -0,0 +1,21 @@
+class Dragon {
+  constructor(name, rider, color, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+  if (hungry === undefined) {
+    this.hungry = true;
+  } else {
+    this.hungry = hungry;
+  }
+  }
+  eat() {
+    for (var i = 0; i < 3; i++) {
+      
+    }
+  }
+}
+
+
+
+module.exports = Dragon;
