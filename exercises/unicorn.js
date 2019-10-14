c7364ea6a1bddc7ce0f1c47634155de66b1d1524
Noah Gambee <noah@gambee.dev>
2019-10-13 20:36:27 -0600

    Finish unicorn testing

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,6 +1,25 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color) {
+    this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
+  }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+  says(quote) {
+    return `**;* ${quote} *;**`
   }
 }
 
 module.exports = Unicorn;
+
+// 1. read the test
+// 2. ask yourself if it is going to fail and why
+// 3. run the test
+// 4. make it pass
