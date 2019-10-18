9123d322a9dad99467741bbdd107430ecf2f214b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-10-18 16:12:27 -0600

    100% Unicorn tests complete

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,5 +1,13 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color || 'white';
+  }
+  isWhite() {
+    if (this.color !== 'white') return false;
+  }
+  says(statement) {
+    return `**;* ${statement} *;**`
   }
 }
 
